e90e7f4dcbf1f028b52d54167734e23726b44601
Jered Linares, MD <JeredL@gmail.com>
2020-06-11 02:52:44 +0000

    roll draft 1

--- dice.rb
+++ dice.rb
@@ -3,5 +3,9 @@ require "sinatra/reloader"
 def view(template); erb template.to_sym; end
 
 get "/" do
-  "3 4"
+    @die1 = rand(1..6)
+    @die2 = rand(1..6)
+
+#  "#{die1} #{die2}"
+    view "dice"
 end
